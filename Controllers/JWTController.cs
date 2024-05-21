90f21763dc6fa539de76ddd0e6b9494b96a39cfe
cangjie <cangjie@hotmail.com>
2024-05-21 17:44:44 +0800

    test rsa

---
+++ Controllers/JWTController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System.Collections.Generic;
+//using JWT.Builder;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace SampleMvcApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class JWTController:ControllerBase
+	{
+		public const string plainTextSecurityKey = "test key";
+
+		public JWTController()
+		{
+		}
+
+		[HttpGet]
+		public void Test()
+		{
+
+
+
+            //X509Certificate  cert = X509Certificate2.CreateFromCertFile(Environment.CurrentDirectory + "/certificate.crt");
+
+            var payload = new Dictionary<string, object>
+            {
+                { "claim1", 0 },
+                { "claim2", "claim2-value" }
+            };
+
+            //var payloadBytes = GetBytes(_jsonSerializer.Serialize(payload));
+
+            IJwtAlgorithm algorithm = new RS256Algorithm((new X509Certificate2(Environment.CurrentDirectory + "/cert.crt")).GetRSAPrivateKey()) ;
+
+
+
+            IJsonSerializer serializer = new JsonNetSerializer();
+
+            byte[] payloadBytes = System.Text.Encoding.UTF8.GetBytes(serializer.Serialize(payload));
+
+            
+            
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            //string key = System.IO.File.OpenText(Environment.CurrentDirectory + "/private.key").ReadToEnd();
+            //byte[] keybArr = System.Text.Encoding.UTF8.GetBytes(key);
+            const string key = null;
+            //byte[] sign = algorithm.Sign(key, payloadBytes) ;
+            var token = encoder.Encode(payload, key);
+
+            Console.WriteLine(token);
+            //Console.WriteLine(token);
+
+
+        }
+
+        
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -8,11 +8,14 @@ using SampleMvcApp.Support;
 using System.Net;
 
 using Microsoft.IdentityModel.Logging;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //To use MVC we have to explicitly declare we are using it. Doing so will prevent a System.InvalidOperationException.
 builder.Services.AddControllersWithViews();
+
+//builder.WebHost.UseStartup<SampleMvcApp.Startup>();
 builder.Services.AddAuth0WebAppAuthentication(options =>
 {
     options.Domain = builder.Configuration["Auth0:Domain"];
@@ -21,6 +24,10 @@ builder.Services.AddAuth0WebAppAuthentication(options =>
 
 // Configure the HTTP request pipeline.
 builder.Services.ConfigureSameSiteNoneCookies();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWT Test", Version = "v1" });
+});
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -29,6 +36,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWT Test v1"));
 
 app.UseStaticFiles();
 app.UseCookiePolicy();
